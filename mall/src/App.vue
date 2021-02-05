ab43fd247d6c8c59ecb2b3e48b9057585d364148
cPopd <1095937663@qq.com>
2021-02-05 18:32:58 +0800

    111

--- mall/src/App.vue
+++ mall/src/App.vue
@@ -5,8 +5,13 @@
 </template>
 
 <script>
+import storage from './storage'
 export default {
-
+  mounted() {
+    // storage.setItem('a', 1)
+    // storage.setItem('user', {a: 1})
+    storage.setItem('abc', {a:1}, 'user')
+  }
 }
 </script>
 
---
+++ mall/src/storage/index.js
@@ -0,0 +1,38 @@
+// storage 封装
+const STORAGE_KEY = 'mall'
+export default {
+  // 存储值
+  setItem(key, value, moudle_name) {
+    if (moudle_name) {
+      let val = this.getItem(moudle_name)
+      val[key] = value
+      this.setItem(moudle_name, val)
+    } else {
+      let val = this.getStorage()
+      val[key] = value
+      window.sessionStorage.setItem(STORAGE_KEY, JSON.stringify(val))
+    }
+  },
+  // 获取值 获取有一个模块下面的属性 user下面的username
+  getItem(key, moudle_name) {
+    if (moudle_name) {
+      let val = this.getItem(moudle_name)
+      if (val) return val[key]
+    }
+   return this.getStorage()[key]
+  },
+  // 获取整个数据
+  getStorage() {
+    return JSON.parse(window.sessionStorage.getItem(STORAGE_KEY) || '{}')
+  },
+  // 清空
+  clear(key, moudle_name) {
+    let val = this.getStorage()
+    if (moudle_name) {
+      delete val[moudle_name][key]
+    } else {
+      delete val[key]
+    }
+    window.sessionStorage.setItem(STORAGE_KEY, JSON.stringify(val))
+  }
+}
